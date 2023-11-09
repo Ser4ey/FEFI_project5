6ffa38f0e60fb53780a382fccfbaa3f64143a388
Sergey <pristup2040@gmail.com>
2023-11-09 12:01:33 +1000

    add first unit test

---
+++ database/test_auth.py
@@ -0,0 +1,51 @@
+import os
+import unittest
+
+from database.auth import AuthDB
+
+
+class TestAuthDB(unittest.TestCase):
+    def setUp(self):
+        # self.db = AuthDB(':memory:')  # Use an in-memory database for testing
+        self.db = AuthDB('test.db')  # Use an in-memory database for testing
+        # self.db.create_auth_table()
+
+    def tearDown(self):
+        self.db.connection.close()
+        if os.path.exists('test.db'):
+            os.remove('test.db')
+        else:
+            print("The file does not exist")
+
+    def test_create_auth_table(self):
+        self.db.create_auth_table()
+        result = self.db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Auth'", fetchone=True)
+        # print(result)
+        self.assertEqual(result[0], 'Auth')
+
+    def test_add_password(self):
+        self.db.add_password('test_password')
+        result = self.db.select_all_passwords()
+        # print(result)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0][1], 'test_password')
+
+    def test_select_password(self):
+        self.db.add_password('test_password')
+        result = self.db.select_password(password='test_password')
+        self.assertEqual(result[1], 'test_password')
+
+    def test_count_passwords(self):
+        self.db.add_password('test_password')
+        self.assertEqual(self.db.count_passwords(), 1)
+
+    def test_update_password(self):
+        self.db.add_password('test_password')
+        self.db.update_password(1, 'new_password')
+        result = self.db.select_password(id=1)
+        self.assertEqual(result[1], 'new_password')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
