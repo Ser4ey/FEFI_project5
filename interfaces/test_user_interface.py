4d4153bf4a61f222cbb350c9fd3197bfdd462aa9
Sergey <pristup2040@gmail.com>
2023-11-14 20:54:18 +1000

    add test_del_desk tests

--- interfaces/test_user_interface.py
+++ interfaces/test_user_interface.py
@@ -24,10 +24,48 @@ class TestUserInterface(unittest.TestCase):
         self.assertFalse(2 == 3)
         self.assertTrue(2+3 > 1)
 
-    # def test_some_exception(self):
-    #     with self.assertRaises(UserInterfaceExceptions.InvalidDeskIdType):
-    #         self.UserInterface.get_deck_by_desk_id('not int')
+    def test_del_desk(self):
+        self.UserInterface.DeskAPI.add_desk("new_desk")
 
+        desk = self.UserInterface.get_desk_by_desk_id(1)
+        self.assertIsNotNone(desk)
+
+        self.UserInterface.add_column_to_desk(1, "column1")
+        self.UserInterface.add_column_to_desk(1, "column2")
+
+        self.assertEqual(len(self.UserInterface.get_columns_by_desk_id(1)), 2)
+        self.UserInterface.del_desk(1)
+
+        with self.assertRaises(UserInterfaceExceptions.DeskNotExist):
+            self.UserInterface.get_columns_by_desk_id(1)
+
+        columns = self.UserInterface.ColumnsAPI.get_columns_by_desk_id(1)
+        self.assertEqual(len(columns), 0)
+
+    def test_del_desk2(self):
+        self.UserInterface.DeskAPI.add_desk("new_desk")
+        self.UserInterface.add_column_to_desk(1, "column1")
+
+        self.assertEqual(len(self.UserInterface.get_columns_by_desk_id(1)), 1)
+        self.UserInterface.add_card_to_column("new_card", 1)
+        self.assertEqual(len(self.UserInterface.get_cards_by_column_id(1)), 1)
+
+        self.UserInterface.del_desk(1)
+        with self.assertRaises(UserInterfaceExceptions.DeskNotExist):
+            self.UserInterface.get_columns_by_desk_id(1)
+        with self.assertRaises(UserInterfaceExceptions.ColumnNotExist):
+            self.UserInterface.get_cards_by_column_id(1)
+
+        columns = self.UserInterface.ColumnsAPI.get_columns_by_desk_id(1)
+        self.assertEqual(len(columns), 0)
+
+        cards = self.UserInterface.CardsAPI.get_cards_by_column_id(1)
+        self.assertEqual(len(cards), 0)
+
+
+
+        columns = self.UserInterface.ColumnsAPI.get_columns_by_desk_id(1)
+        self.assertEqual(len(columns), 0)
 
 if __name__ == '__main__':
     unittest.main()
